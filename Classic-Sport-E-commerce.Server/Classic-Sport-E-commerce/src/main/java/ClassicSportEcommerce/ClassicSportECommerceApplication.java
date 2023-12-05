1393b53c8f16f086e32ff2659635fb3fc1ef76d2
Antonio Colaierà <colaiera@gmail.com>
2023-12-05 11:09:45 +0100

    First commit

---
+++ Classic-Sport-E-commerce.Server/Classic-Sport-E-commerce/src/main/java/ClassicSportEcommerce/ClassicSportECommerceApplication.java
@@ -0,0 +1,13 @@
+package ClassicSportEcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicSportECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicSportECommerceApplication.class, args);
+	}
+
+}
---
+++ Classic-Sport-E-commerce.Server/Classic-Sport-E-commerce/src/test/java/ClassicSportEcommerce/ClassicSportECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ClassicSportEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicSportECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
