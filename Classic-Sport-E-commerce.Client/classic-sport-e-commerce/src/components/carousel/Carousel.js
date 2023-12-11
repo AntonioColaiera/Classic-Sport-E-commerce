b692c54e9970c2b8fa98f35bede665104d53ea64
Antonio Colaierà <colaiera@gmail.com>
2023-12-11 17:08:15 +0100

    carousel

--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/carousel/Carousel.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/carousel/Carousel.js
@@ -1,44 +1,30 @@
 import React, { useState } from "react";
-import Prova1 from "../../assets/carousel/Prova1.jpg";
-import Prova2 from "../../assets/carousel/prova2.jpg";
+import { CarouselItems } from "./CarouselItems";
 import "../../styles/Carousel.css";
 
 export default function Carousel() {
-  const items = [
-    {
-      title: "pROVA",
-      description: "pROVAAAAA",
-      image: Prova1,
-      link: "",
-    },
-    {
-      title: "pROVA2",
-      description: "pROVAAAAA2",
-      image: Prova2,
-      link: "",
-    },
-  ];
-
   const [activeIndex, setActiveIndex] = useState(0);
 
   const goNext = () => {
-    setActiveIndex((activeIndex + 1) % items.length);
+    setActiveIndex((activeIndex + 1) % CarouselItems.length);
   };
 
   const goPrev = () => {
-    setActiveIndex((activeIndex - 1 + items.length) % items.length);
+    setActiveIndex((activeIndex - 1 + CarouselItems.length) % CarouselItems.length);
   };
 
   return (
     <div className="carousel">
+      <div className="carousel-item" style={{backgroundImage: `url(${CarouselItems[activeIndex].image})`}}>
       <button onClick={goPrev}>Prev</button>
-      <div className="carousel-item" style={{backgroundImage: `url(${items[activeIndex].image})`}}>
         <div className="carousel-content">
-          <h2>{items[activeIndex].title}</h2>
-          <p>{items[activeIndex].description}</p>
+          <h2>{CarouselItems[activeIndex].title}</h2>
+          <p>{CarouselItems[activeIndex].description}</p>
+          <bottom>Prova</bottom>
+          <button onClick={goNext}>Next</button>
         </div>
       </div>
-      <button onClick={goNext}>Next</button>
+     
     </div>
   );
 }
---
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/carousel/CarouselItems.js
@@ -0,0 +1,17 @@
+import Prova1 from "../../assets/carousel/Prova1.jpg";
+import Prova2 from "../../assets/carousel/prova2.jpg";
+
+export const CarouselItems = [
+  {
+    title: "pROVA",
+    description: "pROVAAAAA",
+    image: Prova1,
+    link: "",
+  },
+  {
+    title: "pROVA2",
+    description: "pROVAAAAA2",
+    image: Prova2,
+    link: "",
+  },
+];
