70ffd373b97c411bab9a73a5321e648f1e92a4bb
Antonio Colaierà <colaiera@gmail.com>
2023-12-12 17:56:13 +0100

    Registration Form

--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/authentication/Authentication.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/authentication/Authentication.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
+import Modal from "react-modal";
+import "../../styles/Authentication.css"; // Importa il file CSS appena creato
 
 const Authentication = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div>
-      <FaUser />
+      <FaUser onClick={openModal} />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="react-modal-content"
+        overlayClassName="react-modal-overlay"
+      >
+        <h2>Login</h2>
+        <button onClick={closeModal}>Close</button>
+        <div>
+          {/* Il tuo form di login/registrazione va qui */}
+        </div>
+      </Modal>
     </div>
   );
 };
