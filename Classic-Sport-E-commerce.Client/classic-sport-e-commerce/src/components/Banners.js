13e8ce990c0520cd183acd8772940f87315959ef
Antonio Colaierà <colaiera@gmail.com>
2023-12-12 14:47:36 +0100

    Banners

--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/Banners.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/Banners.js
@@ -8,27 +8,28 @@ const Banner = ({ icon, title, subtitle, iconClassName }) => (
   <div className='banner'>
     <div className='banner-content'>
       <img src={icon} alt={title} className={`banner-icon ${iconClassName}`} />
+      </div>
       <div>
         <h2>{title}</h2>
         <p>{subtitle}</p>
       </div>
     </div>
-  </div>
+  
 );
 
 export default function Banners() {
   const bannersData = [
     {
-      icon: MadeInItaly,
-      title: "Made in Italy",
-      subtitle: "Premium materials and design",
-      iconClassName: "made-in-italy-icon",
-    },
-    {
       icon: Research,
       title: "Historical sports research",
       subtitle: "Made by professionals",
       iconClassName: "history-research-icon",
+    },
+    {
+      icon: MadeInItaly,
+      title: "Design made in Italy",
+      subtitle: "Premium materials",
+      iconClassName: "made-in-italy-icon",
     },
     {
       icon: Payments,
@@ -41,7 +42,10 @@ export default function Banners() {
   return (
     <div className='banner-container'>
       {bannersData.map((banner, index) => (
-        <Banner key={index} {...banner} />
+        <React.Fragment key={index}>
+          <Banner {...banner} />
+          {index < bannersData.length - 1 && <div className="vertical-separator" />}
+        </React.Fragment>
       ))}
     </div>
   );
