eb0d3adc42651bd0b808a298ed180e47ffbd819b
Antonio Colaierà <colaiera@gmail.com>
2023-12-18 13:53:59 +0100

    redux cart

--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/cart/Cart.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/cart/Cart.js
@@ -2,20 +2,30 @@ import React from 'react';
 import { IoCartOutline } from 'react-icons/io5';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleCart } from '../redux/actions/cartAction';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root'); // Questa riga è necessaria per l'accessibilità
 
 export default function Cart() {
   const dispatch = useDispatch();
-  const cartOpen = useSelector(state => state.cart.cartOpen); // Accedi al valore corretto
+  const cartOpen = useSelector(state => state.cart.cartOpen);
 
   const handleCartClick = () => {
-    console.log('handleCartClick is called');
     dispatch(toggleCart());
   };
 
   return (
     <div>
       <IoCartOutline onClick={handleCartClick} />
-      {cartOpen && <div>Il carrello è aperto!</div>}
+      <Modal
+        isOpen={cartOpen}
+        onRequestClose={handleCartClick}
+        contentLabel="Carrello"
+      >
+        <h2>Il carrello</h2>
+        <button onClick={handleCartClick}>Chiudi</button>
+        <div>Il carrello è aperto!</div>
+      </Modal>
     </div>
   );
 }
