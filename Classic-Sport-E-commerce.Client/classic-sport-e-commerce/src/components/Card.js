101134c4177dfbe770fa200783b8567187de067e
Antonio Colaierà <colaiera@gmail.com>
2023-12-19 15:57:01 +0100

    Cart

---
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/Card.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const Card = ({ item }) => {
+  return (
+    <div className="card">
+      <img src={item.image} alt={item.title} className="card-image" />
+      <div className="card-content">
+        <h2 className="card-title">{item.title}</h2>
+        <p className="card-description">Typology: {item.typology}</p>
+        <p className="card-description">Price: {item.price}</p>
+        {/* Aggiungi altre informazioni dell'item che desideri mostrare */}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/items/Athletics.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/items/Athletics.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Card from "../Card";
 
 export default function Athletics() {
   const [data, setData] = useState(null);
@@ -11,9 +12,13 @@ export default function Athletics() {
 
   return (
     <div>
-      {data
-        ? data.map((item) => <div key={item.id}>{item.information}</div>)
-        : "Loading..."}
+      {data ? (
+        data.map((item) => (
+          <Card key={item.id} item={item} />
+        ))
+      ) : (
+        "Loading..."
+      )}
     </div>
   );
 }
