dbbd2ba77fae32bd1cebe042f1bafd61fdbe6637
Antonio Colaierà <colaiera@gmail.com>
2023-12-22 11:09:00 +0100

    Mui material Cards

--- Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/Card.js
+++ Classic-Sport-E-commerce.Client/classic-sport-e-commerce/src/components/Card.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import '../styles/Card.css'; // Assicurati che il percorso sia corretto
-import { useDispatch } from 'react-redux';
-import { addItem } from './redux/actions/cartAction';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "./redux/actions/cartAction";
+import Button from "@mui/material/Button";
+import MuiCard from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
 
-const Card = ({ item }) => {
+const Cards = ({ item }) => {
   const dispatch = useDispatch();
 
   const handleAddToCart = () => {
@@ -11,22 +16,58 @@ const Card = ({ item }) => {
   };
 
   return (
-    <div className="card">
-     <img src={require(`../assets/items/${item.image}`)} alt={item.title} className="card-image" />
-
-      <div className="card-content">
-        <h2 className="card-title">{item.title}</h2>
-        <p className="card-description">
-          Typology: {item.typology}
-        </p>
-        <p className="card-description">
-          Price: ${item.price}
-        </p>
-        {/* Aggiungi altre informazioni sull'elemento che desideri visualizzare */}
-        <button onClick={handleAddToCart}>Add to Cart</button>
+    <MuiCard sx={{ width: 320, margin: 2 }}>
+      <div>
+        <Typography variant='h6'>{item.title}</Typography>
+        <Typography variant='body1'>{item.typology}</Typography>
+        <IconButton
+          aria-label='bookmark Bahamas Islands'
+          variant='plain'
+          color='inherit'
+          size='small'
+          sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
+        >
+          <BookmarkAdd />
+        </IconButton>
+        <div style={{ paddingTop: "56.25%", position: "relative" }}>
+          <img
+            src={require(`../assets/items/${item.image}`)} 
+            loading='lazy'
+            alt={item.title}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </div>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <div>
+            <Typography variant='body2'>Total price:</Typography>
+            <Typography variant='h5'>${item.price}</Typography>
+          </div>
+          <Button
+            variant='contained'
+            size='medium'
+            color='primary'
+            aria-label='Explore Bahamas Islands'
+            sx={{ alignSelf: "flex-end", fontWeight: 600 }}
+            onClick={handleAddToCart}
+          >
+            Explore
+          </Button>
+        </CardContent>
       </div>
-    </div>
+    </MuiCard>
   );
 };
 
-export default Card;
+export default Cards;
