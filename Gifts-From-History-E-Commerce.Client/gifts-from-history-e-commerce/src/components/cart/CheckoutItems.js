6eb0632704033383a7e11de1ff87ca5f5363ab4c
Antonio Colaierà <colaiera@gmail.com>
2024-01-10 17:52:44 +0100

    CheckoutItems

--- Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/cart/CheckoutItems.js
+++ Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/cart/CheckoutItems.js
@@ -6,9 +6,22 @@ export default function CheckoutItems() {
   // Leggi lo stato cartItems dallo store Redux
   const cartItems = useSelector((state) => state.cart.cartItems);
 
+
+  const calculateTotalPrice = () => {
+    if (!Array.isArray(cartItems)) {
+      return 0;
+    }
+
+    return cartItems.reduce((total, item) => total + item.total, 0);
+  };
+
   return (
-    <div className="checkout-section right-section">
+    <div className="Checkoutitems">
       {/* Mostra gli elementi del carrello in CheckoutItems */}
+
+
+      <p>Total Cart Price: ${calculateTotalPrice()}</p>{" "}
+
       {cartItems.map((item) => (
         <div key={item.id} className='item'>
           <img
@@ -20,6 +33,7 @@ export default function CheckoutItems() {
           <h2>{item.title}</h2> {/* Titolo dell'articolo */}
           <p>Unit Price: ${item.price}</p> {/* Prezzo unitario dell'articolo */}
           <p>Quantity: {item.quantity}</p> {/* Quantità dell'articolo */}
+          <p>Total Price: ${item.total}</p>{" "}
         </div>
       ))}
     </div>
