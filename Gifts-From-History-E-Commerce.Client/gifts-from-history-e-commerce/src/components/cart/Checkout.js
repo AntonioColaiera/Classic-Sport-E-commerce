e74bf3114e2e4df38c44a5ee0eed725ea3045d75
Antonio Colaierà <colaiera@gmail.com>
2024-01-12 11:46:35 +0100

    Paypal Sandbox

--- Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/cart/Checkout.js
+++ Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/cart/Checkout.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Checkout.css'; // Importa il file CSS
 import CheckoutItems from './CheckoutItems';
 import PaymentDetails from './PaymentDetails';
+import Paypal from './Paypal';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js"; 
+
+const initialOptions = {
+  "clientId": "AcTIOxaOFuPeD5K0Nk7eIel4vAwGt0IqcYbfhF7b1H1RitcxuOgdZ94dDSSvZI52W0eQMV9cYUudNUjf", 
+  "currency": "Euro" 
+  
+};
 
 
 export default function Checkout() {
@@ -9,7 +17,11 @@ export default function Checkout() {
     <div className="checkout-container">
       <div className="checkout-section left-section">
         {/* Prima sezione - 65% */}
+        <PayPalScriptProvider options={initialOptions}>
+          <Paypal/>
+        </PayPalScriptProvider>
         <PaymentDetails/>
+        
       </div>
       <div className="checkout-section right-section">
         {/* Seconda sezione - 35% */}
