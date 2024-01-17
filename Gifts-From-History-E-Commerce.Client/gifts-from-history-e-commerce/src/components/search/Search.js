3872e728d91d85874517e34cfaf24c4b1dc0cb02
Antonio Colaierà <colaiera@gmail.com>
2024-01-17 15:56:03 +0100

    Search listener

--- Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/search/Search.js
+++ Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/search/Search.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { IoMdSearch } from 'react-icons/io';
+import './Search.css'; // Aggiungi uno stile CSS per posizionare il campo di ricerca
 
 export default function Search() {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const searchRef = useRef(null);
+
+  const handleSearchIconClick = () => {
+    setIsSearchOpen(!isSearchOpen);
+  };
+
+  const handleSearch = (e) => {
+    // Implementa la logica di ricerca qui, utilizzando il valore di 'e.target.value'
+    console.log('Ricerca:', e.target.value);
+  };
+
+  const handleClickOutside = (e) => {
+    if (searchRef.current && !searchRef.current.contains(e.target)) {
+      setIsSearchOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div><IoMdSearch /></div>
-  )
+    <div className="search-container" ref={searchRef}>
+      <div onClick={handleSearchIconClick}>
+        <IoMdSearch />
+      </div>
+
+      {isSearchOpen && (
+        <div className="search-overlay">
+          <div className="search-input-container">
+            <input
+              type="text"
+              placeholder="Cerca..."
+              onChange={handleSearch}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
+
+
