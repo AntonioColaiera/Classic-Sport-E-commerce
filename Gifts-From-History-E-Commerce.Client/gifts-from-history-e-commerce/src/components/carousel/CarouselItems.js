16c485d844f5adb3389f285e1006ba538b68a2ba
Antonio Colaierà <colaiera@gmail.com>
2024-01-19 11:15:35 +0100

    Update CarouselItems

--- Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/carousel/CarouselItems.js
+++ Gifts-From-History-E-Commerce.Client/gifts-from-history-e-commerce/src/components/carousel/CarouselItems.js
@@ -1,17 +1,33 @@
 import Prova1 from "../../assets/carousel/Prova1.jpg";
 import Prova2 from "../../assets/carousel/prova2.jpg";
+import SocratesSculpture from '../../assets/items/Socratessculpture.jpg';
+import DavidLivingstonposter from '../../assets/items/DavidLivingstonposter.png';
+import MartinLutherT_shirt from '../../assets/items/MartinLutherT-shirt.png';
+
+
+
+
+
+
 
 export const CarouselItems = [
+
+  {
+    title: "pROVA2",
+    description: "pROVAAAAA2",
+    image: SocratesSculpture,
+    link: "1",
+  },
   {
     title: "pROVA",
     description: "pROVAAAAA",
-    image: Prova1,
+    image: DavidLivingstonposter,
     link: "0.1",
   },
   {
     title: "pROVA2",
     description: "pROVAAAAA2",
-    image: Prova2,
+    image: MartinLutherT_shirt,
     link: "1",
   },
 ];
