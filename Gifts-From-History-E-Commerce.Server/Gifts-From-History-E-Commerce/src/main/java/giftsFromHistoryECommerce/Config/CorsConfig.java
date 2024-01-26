8477970ac2c1c36453d7d055c330fb319be5e126
Antonio Colaierà <colaiera@gmail.com>
2024-01-26 10:45:05 +0100

    update backend comments

--- Gifts-From-History-E-Commerce.Server/Gifts-From-History-E-Commerce/src/main/java/giftsFromHistoryECommerce/Config/CorsConfig.java
+++ Gifts-From-History-E-Commerce.Server/Gifts-From-History-E-Commerce/src/main/java/giftsFromHistoryECommerce/Config/CorsConfig.java
@@ -10,13 +10,28 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
+        // Create a configuration source for URL-based CORS configuration.
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        
+        // Create a new CORS configuration.
         CorsConfiguration config = new CorsConfiguration();
+        
+        // Allow credentials to be sent or received in cross-origin requests.
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // Imposta l'origine del tuo frontend React
+        
+        // Add an allowed origin. In this case, it allows requests from a React frontend running locally.
+        config.addAllowedOrigin("http://localhost:3000"); // Set the origin of your React frontend
+        
+        // Allow all headers to be included in the request.
         config.addAllowedHeader("*");
+        
+        // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.) to be used in cross-origin requests.
         config.addAllowedMethod("*");
+        
+        // Register the CORS configuration for all paths.
         source.registerCorsConfiguration("/**", config);
+        
+        // Create and return a new CorsFilter instance with the configured source.
         return new CorsFilter(source);
     }
 }
--- Gifts-From-History-E-Commerce.Server/Gifts-From-History-E-Commerce/src/main/java/giftsFromHistoryECommerce/Repository/CustomerRepository.java
+++ Gifts-From-History-E-Commerce.Server/Gifts-From-History-E-Commerce/src/main/java/giftsFromHistoryECommerce/Repository/CustomerRepository.java
@@ -8,6 +8,10 @@ import giftsFromHistoryECommerce.Model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     
+    // This interface extends JpaRepository, which provides CRUD functionality for the Customer entity.
 
+    // This method signature declares a custom query method that retrieves a Customer entity by email.
+    // It returns an Optional<Customer> object, which may or may not contain a Customer.
+    // The method name follows Spring Data's method naming conventions for query creation.
     Optional<Customer> findByEmail(String email);
 }
